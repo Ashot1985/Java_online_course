c790d15986a845f0476eafb010b8b4d259deec64
Ashot1985 <ashotdavtyan1985@gmail.com>
2023-10-20 14:07:08 +0400

    My first git commit

---
+++ lesson1/Lesson1/src/Lesson1.java
@@ -0,0 +1,44 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Barev");
+        System.out.println("Lesson1");
+
+      byte x = 127;
+        System.out.println(x);
+
+        short y = -4500;
+        System.out.println(y);
+
+        int a = 458996;
+        System.out.println(a);
+
+        long b = 458698745;
+        System.out.println(b);
+
+        float z = 455.4f;
+        double k = 45.4;
+
+        boolean v = true;
+        System.out.println(v);
+        v = false;
+        System.out.println(v);
+
+        char t = '$';
+        System.out.println(t);
+
+        char tt = '\u003F';
+        System.out.println(tt);
+
+        String m = "Barev Hayer";
+        System.out.println(m);
+
+        String firstName = "Ashot";
+        String lastName = "Davtyan";
+
+        System.out.println(firstName + " " + lastName);
+
+
+
+
+    }
+}
